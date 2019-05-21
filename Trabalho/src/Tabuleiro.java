b37ed7b669a3e91b8e6da5311bf5652603a14690
BrenoFischer <42100477+BrenoFischer@users.noreply.github.com>
2019-05-21 13:49:23 -0300

    Update Tabuleiro.java

--- Trabalho/src/Tabuleiro.java
+++ Trabalho/src/Tabuleiro.java
@@ -7,11 +7,8 @@ import javax.imageio.*;
 public class Tabuleiro extends JPanel {
 	private Tabuleiro t=this;
 	private Image tabuleiro=null;
-	private Pino pino1;
-	private Pino pino2;
-	private Pino pino3;
-	private Pino pino4;
-	private Pino pino5;
+	private Pino pinos[]=new Pino[5];
+
 	
 	public Tabuleiro() {
 		try {
@@ -22,11 +19,11 @@ public class Tabuleiro extends JPanel {
 			System.exit(1);
 		}
 		
-		pino1=new Pino("pin0.png",880,880);
-		pino2=new Pino("pin1.png",915,880);
-		pino3=new Pino("pin2.png",950,880);
-		pino4=new Pino("pin3.png",880,930);
-		pino5=new Pino("pin4.png",915,930);
+		pinos[0]=new Pino("pin0.png",880.0,880.0);
+		pinos[1]=new Pino("pin1.png",915.0,880.0);
+		pinos[2]=new Pino("pin2.png",950.0,880.0);
+		pinos[3]=new Pino("pin3.png",880.0,930.0);
+		pinos[4]=new Pino("pin4.png",915.0,930.0);
 
 
 		addMouseListener(new MouseListener() {
@@ -35,7 +32,14 @@ public class Tabuleiro extends JPanel {
     		public void mouseReleased(MouseEvent e) {}
     		public void mouseExited(MouseEvent e) {}
     		public void mouseClicked(MouseEvent e) {
-    			String msg=String.format("x=%d y=%d\n",e.getX(),e.getY());
+/* *********** Teste para a seleção dos pinos ****************************  */
+    			double x=e.getX();
+    			double y=e.getY();
+    			if ((x >= (pinos[0].getXPino()) && x <= (pinos[0].getXPino()+25.0)) && (y >= (pinos[0].getYPino()) && y <= (pinos[0].getYPino()+35.0))) {
+    				System.out.println("Pino 1 Selecionado");
+    			}
+/* ************* Fim teste seleção dos pinos ******************************** */
+    			String msg=String.format("x=%.1f y=%.1f\n",x,y);
      			JOptionPane.showMessageDialog(t,msg);
     		}
     	});
@@ -45,10 +49,10 @@ public class Tabuleiro extends JPanel {
 		super.paintComponent(g);
 		Graphics2D gd2=(Graphics2D) g;
 		gd2.drawImage(tabuleiro,0,0,null);
-		gd2.drawImage(pino1.getImage(),880,880,null);
-		gd2.drawImage(pino2.getImage(),915,880,null);
-		gd2.drawImage(pino3.getImage(),950,880,null);
-		gd2.drawImage(pino4.getImage(),880,930,null);
-		gd2.drawImage(pino5.getImage(),915,930,null);
+		gd2.drawImage(pinos[0].getImage(),880,880,null);
+		gd2.drawImage(pinos[1].getImage(),915,880,null);
+		gd2.drawImage(pinos[2].getImage(),950,880,null);
+		gd2.drawImage(pinos[3].getImage(),880,930,null);
+		gd2.drawImage(pinos[4].getImage(),915,930,null);
 	}
 }
